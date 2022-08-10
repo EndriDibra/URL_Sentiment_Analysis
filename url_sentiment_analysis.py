90e5593b8c2a270765d607f62dbec5a72b7dbdb1
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2022-08-10 23:20:59 +0300

    Update url_sentiment_analysis.py

--- url_sentiment_analysis.py
+++ url_sentiment_analysis.py
@@ -5,17 +5,14 @@ from textblob import TextBlob as tb
 from textblob.sentiments import NaiveBayesAnalyzer
 from newspaper import Article
 
-
 # initializing url address, then as an article object
 url = input("Please enter the url address")
 article = Article(url)
-
 
 # downloading article, parsing into it and preparing it for nlp
 article.download()
 article.parse()
 article.nlp()
-
 
 # summarizing article and analyzing it about its sentiment
 # using Naive Bayes analyzer
